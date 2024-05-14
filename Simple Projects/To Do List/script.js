d5cc8e5dfc301cfac971b7fa24d2f218b00e138c
Ganpat Sundesha <ganpatsundesha47@gmail.com>
2024-05-14 17:05:26 +0530

    new code

---
+++ Simple Projects/To Do List/script.js
@@ -0,0 +1,8 @@
+let task = document.getElementById("task").value
+let form = document.getElementsByTagName("form")
+console.log(task);
+
+formSubmit = (e) => {
+    e.preventDefault()
+    console.log("ssdcbj");
+}
