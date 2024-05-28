4a9484839277a13d70800ad2d7ec3d139684ef2d
Ganpat Sundesha <ganpatsundesha47@gmail.com>
2024-05-28 16:20:16 +0530

    edit functionality Done

--- Simple Projects/To Do List/script.js
+++ Simple Projects/To Do List/script.js
@@ -3,6 +3,8 @@ let errorMassage = document.querySelector(".errorMassage")
 const todoList = document.getElementById("taskBox")
 
 let todoTask = []
+let editMode = false
+let editId = null
 
 function formSubmit(e) {
     e.preventDefault();
@@ -11,17 +13,22 @@ function formSubmit(e) {
         errorMassage.style.display = "block";
     }
     else {
+        if (editMode) {
+            updateTask(editId, value)
+        }
+        else {
+            addTask(value)
+        }
         errorMassage.style.display = "none";
-        addTask(value)
         task.value = ""
         task.focus()
     }
 }
 
-addTask = (value) => {
+const addTask = (value) => {
 
-    const id = todoTask.length + 1
-    todoTask.push({ id, value })
+    const id = Date.now()
+    todoTask.push({ id, value });
 
     const newDiv = document.createElement("div");
     newDiv.classList.add("todo");
@@ -29,8 +36,8 @@ addTask = (value) => {
 
     const img1 = document.createElement("img");
     const img2 = document.createElement("img");
-    img1.setAttribute("class", "edit")
-    img2.setAttribute("class", "delete")
+    img1.setAttribute("class", "edit");
+    img2.setAttribute("class", "delete");
 
     img1.src = "./images/edit.svg";
     img2.src = "./images/delete.svg";
@@ -43,19 +50,42 @@ addTask = (value) => {
     todoList.appendChild(newDiv);
 
     img2.addEventListener("click", () => deleteTask(id))
-    console.log(todoTask);
+    img1.addEventListener("click", () => editTask(id))
 }
 
 
-deleteTask = (id) => {
+function deleteTask(id) {
     const index = todoTask.findIndex(task => task.id === id)
 
     if (index !== -1) {
-        todoTask.splice(index, 1)
+        todoTask.splice(index, 1);
         const taskDiv = document.querySelector(`.todo[data-id='${id}']`)
         if (taskDiv) {
-            todoList.removeChild(taskDiv)
+            todoList.removeChild(taskDiv);
         }
     }
-    console.log(todoTask);
+}
+
+function editTask(id) {
+    let taskToEdit = todoTask.find(todo => todo.id === id)
+    if (taskToEdit) {
+        task.value = taskToEdit.value
+        editMode = true;
+        editId = taskToEdit.id;
+    }
+}
+
+function updateTask(id, newValue) {
+    let taskToEdit = todoTask.find(todo => todo.id === id)
+
+    if (taskToEdit) {
+        taskToEdit.value = newValue
+
+        const taskDiv = document.querySelector(`.todo[data-id='${id}']`)
+        if (taskDiv) {
+            taskDiv.childNodes[2].nodeValue = newValue
+        }
+        editMode = false
+        editId = null
+    }
 }
